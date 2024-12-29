7598c94e1e14ae81271b1c4a7f4b544f9e56c351
codekesh <201117@iiitt.ac.in>
2024-12-29 12:51:08 +0530

    format: unused items removed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Shop from "./Pages/Shop/Shop";
 import Event from "./Pages/Event/Event";
 import News from "./Pages/News/News";
 import About from "./Pages/About/About";
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup.js";
 import "react-toastify/dist/ReactToastify.css";
--- src/Pages/Productpage/Productpage.js
+++ src/Pages/Productpage/Productpage.js
@@ -12,7 +12,6 @@ import {
   Grid,
   Typography,
   Container,
-  Box,
   Divider,
   Paper,
 } from "@mui/material";
