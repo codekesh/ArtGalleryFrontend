fa0f9d6238de64cafcd7231b1dfff90252933520
codekesh <201117@iiitt.ac.in>
2024-12-30 21:44:45 +0530

    feat: added .env and axiosInstance globally

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,8 @@
+// src/api/axiosInstance.js
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL, // Automatically prepends this to all requests
+});
+
+export default axiosInstance;
